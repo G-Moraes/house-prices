7243cbb8069a52aab3bee4f17fed0d110358de0b
G-Moraes <gmoraes@ufrrj.br>
2022-01-30 22:24:08 -0300

    alterando primeiras features que contém NaN

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #%%
+from multiprocessing import pool
 import pandas as pd
 import numpy as np
 
@@ -10,6 +11,44 @@ import matplotlib.pyplot as plt
 %matplotlib inline
 
 pd.set_option('display.float_format', lambda x: '{:.2f}'.format(x))
+
+#%%
+
+def unique(list1):
+     
+    # insert the list to the set
+    list_set = set(list1)
+    # convert the set to the list
+    unique_list = (list(list_set))
+
+    if(all(isinstance(n, not str)) for n in unique_list):
+        
+        unique_list = [item for item in unique_list if not(pd.isnull(item)) == True]
+    
+    return unique_list, len(unique_list)
+
+def counterElements(lista):
+
+    auxLista, _ = unique(lista)
+
+    dicio = dict.fromkeys(auxLista, 0)
+    dicio['NaN'] = 0
+
+    for i, value in enumerate(auxLista):
+        
+        counter = 0
+        
+        for j in range(len(lista)):
+            
+            if lista[j] == auxLista[i] or (value == 'NaN' and np.isnan(lista[j])):
+
+                counter += 1
+    
+        dicio[value] = counter
+
+    dicio['NaN'] = lista.isna().sum()
+
+    return dicio
 
 #%%
 X = pd.read_csv('test.csv')
@@ -35,23 +74,12 @@ print('there are {} columns with one or more "NaN" as values: {}'.format(totalSu
 
 # %%
 
-def unique(list1):
-     
-    # insert the list to the set
-    list_set = set(list1)
-    # convert the set to the list
-    unique_list = (list(list_set))
-
-    if(all(isinstance(n, not str)) for n in unique_list):
-        
-        unique_list = [item for item in unique_list if not(pd.isnull(item)) == True]
-        unique_list.append(np.nan) 
-    
-    return unique_list, len(unique_list)
-
 for i in columnsWNaN:
 
     unique_list, size = unique(X[i])
+
+    unique_list.append(np.nan)
+    size += 1
 
     print('{}: {}, {}'.format(i, size, unique_list))
 
@@ -75,4 +103,33 @@ plt.xticks(rotation = '90')
 plt.xlabel('Features', fontsize = 15)
 plt.ylabel('% de valores faltantes', fontsize = 15)
 plt.title('Porcentagem de valores faltantes por Feature', fontsize = 15)
+
+# %% tratando os faltantes da feature PoolQC
+print(unique(X['PoolQC']))
+
+X['PoolQC'].fillna('NA', inplace = True)
+
+# %% tratando os faltantes da feature MiscFeature
+
+print(counterElements(X['MiscFeature']))
+print(counterElements(X['MiscVal']))
+
+X['MiscFeature'].fillna('NA', inplace = True)
+
+# %% tratando os faltantes da feature Alley
+print(unique(X['Alley']))
+
+X['Alley'].fillna('NA', inplace = True)
+
+# %% tratando os faltantes da feature Fence
+print(unique(X['Fence']))
+
+X['Fence'].fillna('NA', inplace = True)
+
+# %%
+print(counterElements(X['Fireplaces']))
+print(counterElements(X['FireplaceQu']))
+
+X['FireplaceQu'].fillna('NA', inplace = True)
+
 # %%
